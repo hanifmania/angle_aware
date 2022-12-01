d2e04de2a32cf8fce3f05a36f791002a7e48f479
shimita <tashiwater@gmail.com>
2022-12-01 12:10:11 +0900

    show apple

--- angle_aware_switch/src/angle_aware_switch/angle_aware_agent.py
+++ angle_aware_switch/src/angle_aware_switch/angle_aware_agent.py
@@ -5,11 +5,15 @@ from angle_aware_control.myqp import MyQP
 from angle_aware_control.psi_generator_no_jax import zeta_func
 from coverage_util.field_generator import FieldGenerator
 
-import numpy as np
 import rospy
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Bool
+
 import copy
+import numpy as np
+
+from jsk_rviz_plugins.msg import Pictogram
+import matplotlib.pyplot as plt
 
 
 class Agent:
@@ -18,27 +22,34 @@ class Agent:
         input_detect_topic = rospy.get_param(
             "~input_detect_topic", default="grape_posestamped"
         )
+        output_pictogram = rospy.get_param("~output_pictogram", default="grape")
+
+        self._world_tf = rospy.get_param("~world", default="world")
         self._grape_size = rospy.get_param("/grape_detector/size", default=None)
         self.agentID = rospy.get_param("agentID", default=-1)
         field_cbf = rospy.get_param("/field_cbf")
         agents_param = rospy.get_param("/agents")
         angle_aware_params = rospy.get_param("~angle_aware", default=None)
-        self._clock = agents_param["agent_manager_clock"]
-        self._umax = agents_param["u_max"]
         collision_distance = rospy.get_param("collision_distance")
+
+        self._clock = agents_param["agent_manager_clock"]
         self._kp_z = agents_param["kp_z"]
         self._ref_z = agents_param["ref_z"]
         self._kp_yaw = agents_param["kp_yaw"]
         self._ref_yaw = agents_param["ref_yaw"]
+        self._umax = angle_aware_params["u_max"]
         self._phi_param = angle_aware_params["phi"]
-        self._threshold = angle_aware_params["threshold"]
+        self._threshold_rate = angle_aware_params["threshold_rate"]
         self._delta_decrease = angle_aware_params["delta_decrease"]
         self._sigma = angle_aware_params["sigma"]
+        self._observe_time = angle_aware_params["observe_time"]
+        self._pictogram_param = angle_aware_params["pictogram"]
 
         phi_generator = FieldGenerator(self._phi_param)
         self._A = phi_generator.get_point_dense()
 
         self._phi_A = 0
+        self._phi_0 = 1.0
         self._grape_queue = []
         self._dt = 1.0 / self._clock
 
@@ -46,6 +57,12 @@ class Agent:
         self._qp = myqp(field_cbf, collision_distance, angle_aware_params)
 
         self._pub_flag = rospy.Publisher(flag_topic, Bool, queue_size=1)
+        self._pub_pictogram = rospy.Publisher(
+            output_pictogram,
+            Pictogram,
+            queue_size=1,
+        )
+
         rospy.Subscriber(input_detect_topic, PoseStamped, self.grape_callback)
 
         self._agent_base.wait_pose_stamped()
@@ -125,6 +142,7 @@ class Agent:
                 self._pub_flag.publish(is_angle_aware)
                 if is_angle_aware:
                     self.main_control()
+                    self.show_pictogram()
             rate.sleep()
 
     ###################################################################
@@ -144,7 +162,7 @@ class Agent:
             bool: true if angle_aware
         """
 
-        angle_aware_mode = np.sum(self._phi_A) > self._threshold
+        angle_aware_mode = np.sum(self._phi_A) > self._phi_0 * self._threshold_rate
         if angle_aware_mode:
             ### まだangle awareすべき
             return True
@@ -153,14 +171,17 @@ class Agent:
             self._phi_A = 0
             return False
 
-        ### target fieldを新しくして再検証
-        grape_posestamped = self._grape_queue.pop()
+        ### target fieldを新しく生成
+        self._grape_posestamped = self._grape_queue.pop()
         self._phi_A, self._zeta = self.generate_q(
-            grape_posestamped, self._phi_param, self._grape_size, self._ref_z
+            self._grape_posestamped, self._phi_param, self._grape_size, self._ref_z
         )
 
-        rospy.loginfo("max J : {}".format(np.sum(self._phi_A)))
-        return self.judge_angle_aware()
+        self._phi_0 = np.sum(self._phi_A)
+        gamma = self._phi_0 / self._observe_time
+        rospy.loginfo("gamma : {}".format(gamma))
+        self._qp._angle_aware_cbf.set_gamma(gamma)
+        return True
 
     def generate_q(self, posestamped, param_base, grape_size, ref_z):
         x = posestamped.pose.position.x
@@ -190,6 +211,33 @@ class Agent:
         phi -= delta_decrease * h * phi * dt
         # print(np.sum(self._delta_decrease * h_max * self._psi * self._dt))
         return (0 < phi) * phi  ## the minimum value is 0
+
+    def show_pictogram(self):
+        color_seed = np.sum(self._phi_A) / self._phi_0
+        color_rgba = plt.get_cmap("jet")(color_seed)
+
+        msg = Pictogram()
+        msg.action = Pictogram.ROTATE_X
+        msg.header.frame_id = self._world_tf
+        msg.header.stamp = rospy.Time.now()
+
+        msg.pose.position.x = self._grape_posestamped.pose.position.x
+        msg.pose.position.y = self._grape_posestamped.pose.position.y
+        msg.pose.position.z = self._grape_posestamped.pose.position.z
+        msg.pose.orientation.w = 0.7
+        msg.pose.orientation.x = 0
+        msg.pose.orientation.y = -0.7
+        msg.pose.orientation.z = 0
+        msg.mode = Pictogram.PICTOGRAM_MODE
+        msg.speed = 0.5
+        # msg.ttl = 5.0
+        msg.size = self._pictogram_param["size"]
+        msg.color.r = color_rgba[0]
+        msg.color.g = color_rgba[1]
+        msg.color.b = color_rgba[2]
+        msg.color.a = color_rgba[3]
+        msg.character = self._pictogram_param["character"]
+        self._pub_pictogram.publish(msg)
 
 
 if __name__ == "__main__":
