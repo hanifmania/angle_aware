1ab5b7e0cf6205c66f0eb4f72e8c61632fd0f394
shimita <tashiwater@gmail.com>
2023-01-13 23:10:44 +0900

    add angle_aware_avoid_tree for 0226 experiment

--- angle_aware_avoid_tree/src/angle_aware_avoid_tree/agent.py
+++ angle_aware_avoid_tree/src/angle_aware_avoid_tree/agent.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from angle_aware_control.agent import Agent
-from angle_aware_avoid_tree.myqp import QPAvoidTree
+from angle_aware_control.agent_with_unom import Agent
+from angle_aware_control.myqp import MyQP
 import rospy
 
 if __name__ == "__main__":
     rospy.init_node("agent", anonymous=True)
     trees = rospy.get_param("/trees")
-    agent = Agent(QPAvoidTree)
-    agent._qp.set_params(trees)
+    agent = Agent(MyQP)
+    agent._qp.set_obstacle_avoidance_param(trees)
     agent.spin()
